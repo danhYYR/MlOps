233174dec6e4b46d07e52c10f2cdc5d3ff525af2
nguyen332 <danhcong.nguyen@dxc.com>
2024-02-23 09:07:09 +0000

    add output for resource

--- terraform/modules/jumphost/output.tf
+++ terraform/modules/jumphost/output.tf
@@ -1,4 +1,16 @@
 output "admin_username" {
-    description = "The admin user of the jumphost machine"
-    value = azurerm_linux_virtual_machine.jp_vm.admin_username
+  description = "The admin user of the jumphost machine"
+  value       = azurerm_linux_virtual_machine.jp_vm.admin_username
+}
+locals {
+  ssh_key = split(".", var.ssh_key)[0]
+}
+# Output jumphost information to a file (securely store this file)
+output "jumphost_data" {
+    value = {
+        hostname = azurerm_linux_virtual_machine.jp_vm.name
+        username = azurerm_linux_virtual_machine.jp_vm.admin_username
+        ssh_key  = local.ssh_key
+    }
+
 }
