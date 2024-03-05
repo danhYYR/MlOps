1637ac43441e05c91832c009a03ea6551ed8cd84
nguyen332 <danhcong.nguyen@dxc.com>
2024-03-05 05:16:55 +0000

    add: add variable when run terrform command

--- infra/terraform/cmd/tf-destroy.sh
+++ infra/terraform/cmd/tf-destroy.sh
@@ -1,1 +1,6 @@
-terraform destroy --var-file tfvars/dev.tfvars -var tenant_id=$TENANT_ID  -var sp_id=$SP_ID -var sp_secret=$SP_SECRET --auto-approve 
+terraform destroy \
+    -var-file tfvars/dev.tfvars \
+    -var tenant_id=$TENANT_ID  -var sp_id=$SP_ID \
+    -var sp_secret=$SP_SECRET \
+    -var-file tfvars/sh.tfvars \
+    --auto-approve 
--- infra/terraform/cmd/tf-run.sh
+++ infra/terraform/cmd/tf-run.sh
@@ -1,1 +1,7 @@
-terraform apply -var-file tfvars/dev.tfvars -var tenant_id=$TENANT_ID  -var sp_id=$SP_ID -var sp_secret=$SP_SECRET -auto-approve 
+terraform apply \
+    -var-file tfvars/dev.tfvars \
+    -var tenant_id=$TENANT_ID  \
+    -var sp_id=$SP_ID \
+    -var sp_secret=$SP_SECRET \
+    -var-file tfvars/sh.tfvars \
+    -auto-approve 
